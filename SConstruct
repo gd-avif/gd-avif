d35b18213c8c9dce4d52744e16f404b2cba55f07
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2023-06-21 07:23:49 +0200

    Move the universal build hack to the AOM tool.
    Only do the double build + lipo hack for AOM which does not support
    (optimized) universal builds.
    
    This simplifies the main SConstruct quite a lot, and improve overall the
    build performance for universal builds.

--- SConstruct
+++ SConstruct
@@ -22,6 +22,11 @@ if "android_api_level" not in ARGUMENTS:
 env = SConscript("godot-cpp/SConstruct").Clone()
 
 opts = Variables([], ARGUMENTS)
+
+# Dependencies
+for tool in ["cmake", "aom", "avif"]:
+    env.Tool(tool, toolpath=["tools"])
+
 opts.Update(env)
 
 result_path = os.path.join("bin", "gdavif")
@@ -31,46 +36,15 @@ env.Append(CPPPATH=["src/"])
 sources = []
 add_sources(sources, "src/", "cpp")
 
-mac_universal = env["platform"] == "macos" and env["arch"] == "universal"
-build_targets = []
-build_envs = [env]
+# Make our dependencies
+aom = env.BuildAOM()
+avif = env.BuildLibAvif(aom)
 
-# For macOS universal builds, setup one build environment per architecture.
-if mac_universal:
-    build_envs = []
-    for arch in ["x86_64", "arm64"]:
-        benv = env.Clone()
-        benv["arch"] = arch
-        benv["CCFLAGS"] = SCons.Util.CLVar(str(benv["CCFLAGS"]).replace("-arch x86_64 -arch arm64", "-arch " + arch))
-        benv["LINKFLAGS"] = SCons.Util.CLVar(
-            str(benv["LINKFLAGS"]).replace("-arch x86_64 -arch arm64", "-arch " + arch)
-        )
-        benv["suffix"] = benv["suffix"].replace("universal", arch)
-        benv["SHOBJSUFFIX"] = benv["suffix"] + benv["SHOBJSUFFIX"]
-        build_envs.append(benv)
+# Make the shared library
+result_name = "gdavif{}{}".format(env["suffix"], env["SHLIBSUFFIX"])
+library = env.SharedLibrary(target=os.path.join(result_path, "lib", result_name), source=sources)
 
-for benv in build_envs:
-    # Dependencies
-    for tool in ["cmake", "aom", "avif"]:
-        benv.Tool(tool, toolpath=["tools"])
-
-    aom = benv.BuildAOM()
-    avif = benv.BuildLibAvif(aom)
-
-    # Make the shared library
-    result_name = "gdavif{}{}".format(benv["suffix"], benv["SHLIBSUFFIX"])
-    library = benv.SharedLibrary(target=os.path.join(result_path, "lib", result_name), source=sources)
-    build_targets.append(library)
-
-Default(build_targets)
-
-# For macOS universal builds, join the libraries using lipo.
-if mac_universal:
-    result_name = "libgdavif{}{}".format(env["suffix"], env["SHLIBSUFFIX"])
-    universal_target = env.Command(
-        os.path.join(result_path, "lib", result_name), build_targets, "lipo $SOURCES -output $TARGETS -create"
-    )
-    Default(universal_target)
+Default(library)
 
 # GDNativeLibrary
 extfile = env.InstallAs(os.path.join(result_path, "gdavif.gdextension"), "misc/gdavif.gdextension")
--- tools/aom.py
+++ tools/aom.py
@@ -16,16 +16,61 @@ def aom_cmake_config(env):
     return config
 
 
-def build_library(env):
-    aom_env = env.Clone()
-    aom = env.CMake(
-        [env.Dir(env["AOM_BUILD"])] + env["AOM_LIBS"],
-        env.Dir(env["AOM_SOURCE"]),
-        CMAKECONFFLAGS=["-D%s=%s" % it for it in aom_cmake_config(env).items()],
-    )
+def build_library(env, jobs=None):
+    if jobs is None:
+        jobs = int(env.GetOption("num_jobs"))
+
+    # Since AOM does not support macOS universal binaries, we first need to build the two libraries
+    # separately, then we join them together using lipo.
+    if env["platform"] == "macos" and env["arch"] == "universal":
+        build_envs = {
+            "x86_64": env.Clone(),
+            "arm64": env.Clone(),
+        }
+        arch_aom = []
+        for arch in build_envs:
+            benv = build_envs[arch]
+            benv["arch"] = arch
+            generate(benv)
+            benv["CMAKEBUILDJOBS"] = max([1, int(jobs / len(build_envs))])
+            aom = benv.CMake(
+                [benv.Dir("${AOM_BUILD}")] + benv["AOM_LIBS"],
+                benv.Dir("${AOM_SOURCE}"),
+                CMAKECONFFLAGS=["-D%s=%s" % it for it in aom_cmake_config(benv).items()],
+            )
+            arch_aom.extend(aom)
+            benv.NoCache(aom)  # Needs refactoring to properly cache generated headers.
+
+        aom = []
+        common = ["libaom_version.a", "libaom_pc.a", "libaom.a"]
+        x64_libs = filter(lambda lib: not any([str(lib).endswith(c) for c in common]), build_envs["x86_64"]["AOM_LIBS"])
+        # Join libraries using lipo.
+        lipo_action = "lipo $SOURCES -create -output $TARGET"
+
+        # First the x86-only libraries ("fake" fat libraries).
+        for lib in x64_libs:
+            aom += env.Command(env.File("${AOM_BUILD}/" + os.path.basename(str(lib))), lib, lipo_action)
+
+        # Then the actual fat ones.
+        for lib in common:
+            aom += env.Command(
+                env.File("${AOM_BUILD}/" + lib),
+                [benv.File("${AOM_BUILD}/" + lib) for benv in build_envs.values()],
+                lipo_action,
+            )
+        env.Depends(aom, arch_aom)
+
+    else:
+        aom = env.CMake(
+            [env.Dir("${AOM_BUILD}")] + env["AOM_LIBS"],
+            env.Dir("${AOM_SOURCE}"),
+            CMAKECONFFLAGS=["-D%s=%s" % it for it in aom_cmake_config(env).items()],
+        )
+
     env.Append(LIBPATH=[env["AOM_BUILD"]])
     env.Append(CPPPATH=[env["AOM_INCLUDE"]])
     env.Prepend(LIBS=env["AOM_LIBS"])
+
     return aom
 
 
@@ -38,7 +83,7 @@ def generate(env):
     env["AOM_BUILD"] = env.Dir("#bin/thirdparty/aom/${platform}/${arch}").abspath
     env["AOM_INCLUDE"] = env["AOM_SOURCE"]
     libs = []
-    if env["arch"] == "x86_64":
+    if env["arch"] == "x86_64" or (env["platform"] == "macos" and env["arch"] == "universal"):
         libs += [
             "libaom_av1_encoder_sse2_static.a",
             "libaom_av1_encoder_ssse3_static.a",
